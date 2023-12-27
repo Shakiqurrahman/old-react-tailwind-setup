b8132408efd3801f1606977b85b48c72cb57cbbb
learnwithancor <ancorpoint0@gmail.com>
2023-12-27 21:32:28 +0600

    commited

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-center mt-20'>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
